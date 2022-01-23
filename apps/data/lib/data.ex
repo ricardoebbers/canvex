90f1e46f03342af0ce19c638cc28f58dda0f82c9
Ricardo Leão <ricardo@jaya.tech>
2022-01-23 20:49:57 -0300

    feat: Purge old project

--- apps/data/lib/data.ex
+++
@@ -1,8 +0,0 @@
-defmodule Data do
-  alias Data.Canvas.{Create, Delete, Get, Update}
-
-  defdelegate create_canvas(canvas), to: Create, as: :call
-  defdelegate delete_canvas(id), to: Delete, as: :call
-  defdelegate get_canvas_by_id(id), to: Get, as: :by_id
-  defdelegate update_canvas(id, canvas), to: Update, as: :call
-end
--- apps/data/test/data_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule DataTest do
-  use Data.RepoCase
-  doctest Data
-end

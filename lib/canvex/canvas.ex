fd4ae3e151a385ad32108cb1deb032e67a909795
Ricardo Leão <ricardo@jaya.tech>
2022-01-24 13:13:29 -0300

    feat: Add public API for canvas operations

--- lib/canvex/canvas.ex
+++ lib/canvex/canvas.ex
@@ -1,2 +1,12 @@
-defmodule Canvex.Draw do
+defmodule Canvex.Canvas do
+  alias Canvex.Canvas.{Create, Get, Update}
+  alias Canvex.Draw.{FloodFill, Rectangle}
+
+  defdelegate new_canvas(attrs), to: Create, as: :call
+
+  defdelegate get_canvas_by_id(id), to: Get, as: :by_id
+  defdelegate draw_rectangle(canvas, attrs), to: Rectangle, as: :call
+
+  defdelegate flood_fill(canvas, attrs), to: FloodFill, as: :call
+  defdelegate update_canvas(canvas), to: Update, as: :call
 end

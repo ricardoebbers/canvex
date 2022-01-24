0e195cd3552ed766a1dd8c45afc13960268681da
Ricardo Leão <ricardo@jaya.tech>
2022-01-24 18:16:42 -0300

    refactor: Move API to Canvex

--- lib/canvex.ex
+++ lib/canvex.ex
@@ -6,4 +6,14 @@ defmodule Canvex do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias Canvex.Canvas.{Create, Get, Update}
+  alias Canvex.Draw.{FloodFill, Rectangle}
+
+  defdelegate new_canvas(attrs), to: Create, as: :call
+
+  defdelegate get_canvas_by_id(id), to: Get, as: :by_id
+  defdelegate draw_rectangle(canvas, attrs), to: Rectangle, as: :call
+
+  defdelegate flood_fill(canvas, attrs), to: FloodFill, as: :call
+  defdelegate update_canvas(canvas), to: Update, as: :call
 end

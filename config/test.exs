52f184e33dad8399ec8cb1b1c28363a3ed866f30
Ricardo Leão <ricardo@jaya.tech>
2022-01-25 23:54:03 -0300

    Update elixir.yml

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,12 @@ config :canvex, Canvex.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
+if System.get_env("GITHUB_ACTIONS") do
+  config :canvex, Canvex.Repo,
+    username: "postgres",
+    password: "postgres"
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :canvex, CanvexWeb.Endpoint,
